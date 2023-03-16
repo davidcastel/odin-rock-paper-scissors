bbd35ee1e303f1f1370eeeeffd7ba95434c5591b
davidcastel <davidccbp20@gmail.com>
2023-03-15 22:52:59 -0400

    create playRound method

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ let getComputerChoice = () => {
             return "Scissor";
     }
 }
+
+let playRound = (playerSelection, computerSelection) => {
+    // make parameters case-insensitive
+    playerSelection=playerSelection.toLowerCase();
+    computerSelection=computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) return `Tie! ${computerSelection} does not beat ${playerSelection}. Play Again`;
+    else if (
+        (playerSelection === "rock" && computerSelection === "scissor") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissor" && computerSelection === "paper")
+        ) {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+    else return `You Lose! ${computerSelection} beats ${playerSelection}`;
+}
