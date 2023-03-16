621145780f444981dd849bbdf41077e9f3f5cc1e
davidcastel <davidccbp20@gmail.com>
2023-03-15 23:29:24 -0400

    implement game method with score tracking

--- index.js
+++ index.js
@@ -26,3 +26,31 @@ let playRound = (playerSelection, computerSelection) => {
         }
     else return `Loser`;
 }
+
+let game = () => {
+    let responseDir = {"a" : "rock", "b": "paper", "c": "scissor"}
+    let [playerScore, computerScore] = [0,0];
+    let playerAnswer, computerAnswer, winner;
+    
+    for (let i = 0; i < 5; i++) {
+        playerAnswer = window.prompt("Please select an option, A) Rock B) Paper C) Scissor\n*Please Type A, B or C. Any other option will become void").toLocaleLowerCase();
+        if ((playerAnswer === "a") || (playerAnswer === "b") || (playerAnswer === "c")) {
+            computerAnswer = getComputerChoice();
+            winner = playRound(responseDir[playerAnswer], computerAnswer);
+            switch(winner) {
+                case `Winner`:
+                    playerScore++;
+                    break;
+                case `Loser`:
+                    computerScore++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        else i--;
+    }
+    if (playerScore == computerScore) return `There is a tie!`;
+    else if (playerScore > computerScore) return `Player is the winner`;
+    else return `Computer is the winner`;
+}
